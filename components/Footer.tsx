31d14310d0bab617859edfb0e44d009b5ce54157
Marcin Siepetowski <siepet7@gmail.com>
2023-11-21 18:43:48 +0100

    Do footer styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link'
+
 export default function Footer() {
+  const year = new Date().getFullYear()
+
   return (
-    <footer className="mt-auto bottom-0 w-full text-sm text-center mb-1">
-      marcin siepetowski 2023
+    <footer className="mt-auto bottom-0 w-full text-sm text-center mb-1 flex justify-evenly">
+      <Link href="/about">informacje</Link>
+      <span className="mx-1">marcin siepetowski {year}</span>
+      <Link href="/">zgadywanka</Link>
     </footer>
   )
 }
